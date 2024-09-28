2f220b76fa67034b2135bcd1ded14f9e5b81d380
Tyr Chen <tyr.chen@gmail.com>
2024-09-28 15:27:01 -0700

    feature: improve send component

--- chatapp/src/components/MessageSend.vue
+++ chatapp/src/components/MessageSend.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col p-4 bg-white border-t border-gray-200">
+  <div class="flex flex-col bg-gray-100 border-t border-gray-200 relative bottom-0">
     <div class="flex items-center">
       <button @click="triggerFileUpload" class="p-2 mr-2 text-gray-600 hover:text-blue-600 focus:outline-none">
         <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -14,41 +14,45 @@
         accept="image/*"
         class="hidden"
       />
-      <input
+      <!-- Add more image buttons here if needed -->
+    </div>
+
+    <div>
+      <textarea
         v-model="message"
         @keyup.enter="sendMessage"
         placeholder="Type a message..."
-        class="flex-1 px-4 py-3 mr-2 text-sm bg-gray-100 border-none rounded-lg focus:outline-none"
-        type="text"
-      />
-      <div v-if="files.length > 0" class="flex mt-2 space-x-2 overflow-x-auto">
-        <img
-          v-for="file in files"
-          :key="file.path"
-          :src="file.fullUrl"
-          class="w-16 h-16 object-cover rounded"
-          alt="Uploaded image"
-        />
-      </div>
-      <button @click="sendMessage" class="p-2 text-white bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-5 h-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M5 12h14M12 5l7 7-7 7"
-          />
-        </svg>
-      </button>
-
+        class="w-full px-4 text-sm bg-gray-100 border-none rounded-lg focus:outline-none resize-none"
+        rows="3"
+      ></textarea>
     </div>
 
+    <div v-if="files.length > 0" class="flex flex-wrap p-2">
+      <img
+        v-for="file in files"
+        :key="file.path"
+        :src="file.fullUrl"
+        class="h-64 object-cover rounded mr-2 mb-2"
+        alt="Uploaded image"
+      />
+    </div>
+
+    <button @click="sendMessage" class="absolute bottom-4 right-4 p-1 text-white bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-5 h-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M5 12h14M12 5l7 7-7 7"
+        />
+      </svg>
+    </button>
   </div>
 </template>
 
